78e75fe455f43cd665d1575b283e882626aa1f95
ahmedss1997 <asami6596@gmail.com>
2024-04-09 00:39:28 +0200

    upload dist

--- .netlify/functions-internal/server/chunks/app/_nuxt/login-styles.8e7c7d65.mjs
+++
@@ -1,6 +0,0 @@
-const login_vue_vue_type_style_index_0_lang = ".signin{background-size:cover}.signin .overlay{background:linear-gradient(90deg,rgba(0,0,0,.5),rgba(17,1,31,.9));height:100%;width:100%}";
-
-const loginStyles_8e7c7d65 = [login_vue_vue_type_style_index_0_lang, login_vue_vue_type_style_index_0_lang];
-
-export { loginStyles_8e7c7d65 as default };
-//# sourceMappingURL=login-styles.8e7c7d65.mjs.map
--- .netlify/functions-internal/server/server.mjs
+++
@@ -1,6 +0,0 @@
-globalThis._importMeta_={url:import.meta.url,env:process.env};export { F as handler } from './chunks/nitro/netlify.mjs';
-import 'node:http';
-import 'node:https';
-import 'fs';
-import 'path';
-//# sourceMappingURL=server.mjs.map
